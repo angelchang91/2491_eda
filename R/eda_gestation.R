df7f0404f8087cdf3726984ff5dd78c40557aa7d
Angel Chang <angel.h.chang@gmail.com>
2024-01-11 11:03:41 +0000

    wip

--- R/eda_gestation.R
+++ R/eda_gestation.R
@@ -21,25 +21,33 @@ count(Gestation)
 count(Gestation, race)
 
 # number of observations by racial group and level of mother's education
-Gestation_n_race_ed <- count(Gestation, ...)
+Gestation_n_race_ed <- count(Gestation, ed, race)
 
 
 # Activity 2 - Further summary statistics
+#create new Gestation df without Na values in age column
 
+
+count(Gestationc)
 # mean age of mothers across all births
 # ensure you use a human friendly name for the value you're creating
 
 # calculate both mothers' mean age and babies' mean weight
-summarise(Gestation, 
-          `Mean age` = ...,
-          `Mean wt`  = ...)
-
+summarise(Gestation, meanage = mean(age, na.rm= T), meanbweight = mean(wt, na.rm= T))
 
 # Activity 3 - Grouped summaries
 
 # make a new data frame containing only id, age and race variables
+gestationnew<- subset(Gestation, select=c(age, id, race))
+
+
 
 # calculate the mean age by race
+mean_age_by_race <- Gestation %>%
+  group_by(race) %>%
+  summarise(mean_age = mean(age, na.rm = TRUE))
+
+print(mean_age_by_race)
 
 
 # Activity 4 - Extensions
@@ -48,22 +56,36 @@ summarise(Gestation,
 # Activity 4a - Correlation
 
 # Calculate the correlation between age and weight across all births
-
+cor(Gestation$age, Gestation$wt, use = "complete.obs")
 # Calculate the correlation between age and weight for each race group
 
 
 # Activity 4b - Multiple summary statistics
 
 # Calculate the sample mean of the ages and weights of the mothers in each race group
+mean_age_weight_by_race <- Gestation %>%
+  group_by(race) %>%
+  summarise(mean_age = mean(age, na.rm = TRUE), mean_wt=mean(wt.1, na.rm=TRUE))
+print(mean_age_weight_by_race)
 
 
+mean_age_weight_byrace_at<- Gestation%>% group_by(race)%>% summarise_at(
+  vars(age, wt.1), .funs=list(mean=mean), na.rm=T
+)
+
+print(mean_age_weight_byrace_at)
+
+Gestation<- mutate(Gestation, race=str_to_title(race))
 # Activity 4c - Pivoting wider
 
-# Make a wide table from the summary data frame calculated in Activity 1 that has the number of observations for each combination of mother's education level and race. Make each row is an education level and each column a race group.
+# Make a wide table from the summary data frame calculated in Activity 1 that has the 
+#number of observations for each combination of mother's education level and race. 
+#Make each row is an education level and each column a race group.
 
-# Hint: Look at the help file for `pivot_wider` for what to do with missing cells (where there is no combination of these variables) and set the argument to be 0.
+# Hint: Look at the help file for `pivot_wider` for what to do with missing cells 
+#(where there is no combination of these variables) and set the argument to be 0.
 
-
+pivot_wider(Gestation_n_race_ed)
 # Activity 4d - Multiple summary statistics
 
 # Calculate the mean, standard deviation, minimum, maximum and proportion of values missing for the mothers' ages for each race group.
